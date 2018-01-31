d539bf78a7f7082f0a0e7ae7c31b885dc2924e65
aniruddh-g <31592724+aniruddh-g@users.noreply.github.com>
2018-01-31 12:29:36 +0530

    Create Various Functions.py

---
+++ Various Functions.py
@@ -0,0 +1,14 @@
+def bootstrap_replicate_1d(data, func):
+    return func(np.random.choice(data, size=len(data)))
+
+def draw_bs_reps(data, func, size=1):
+    """Draw bootstrap replicates."""
+
+    # Initialize array of replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_replicates[i] = bootstrap_replicate_1d(data,func,size)
+
+    return bs_replicates
