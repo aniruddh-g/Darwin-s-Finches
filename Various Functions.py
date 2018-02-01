b44172ad714b78ad5536fe0dca63e4ecb745a740
aniruddh-g <31592724+aniruddh-g@users.noreply.github.com>
2018-02-01 10:53:48 +0530

    Update Various Functions.py

--- Various Functions.py
+++ Various Functions.py
@@ -30,3 +30,17 @@ def draw_bs_pairs_linreg(x, y, size=1):
         bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x,bs_y,1)
 
     return bs_slope_reps, bs_intercept_reps
+
+def pearson_r(x,y):
+    """Compute Pearson correlation coefficient between two arrays."""
+    # Compute correlation matrix: corr_mat
+    corr_mat = np.corrcoef(x,y)
+
+    # Return entry [0,1]
+    return corr_mat[0,1]
+
+    # Compute Pearson correlation coefficient for I. versicolor: r
+    r = pearson_r(versicolor_petal_length,versicolor_petal_width)
+
+    # Print the result
+    print(r)
