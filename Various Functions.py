006d70a390d63908c50901d114ab8e4ee3473c63
aniruddh-g <31592724+aniruddh-g@users.noreply.github.com>
2018-02-01 10:39:23 +0530

    Update Various Functions.py

--- Various Functions.py
+++ Various Functions.py
@@ -12,3 +12,21 @@ def draw_bs_reps(data, func, size=1):
         bs_replicates[i] = bootstrap_replicate_1d(data,func,size)
 
     return bs_replicates
+
+def draw_bs_pairs_linreg(x, y, size=1):
+    """Perform pairs bootstrap for linear regression."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_slope_reps, bs_intercept_reps
+    bs_slope_reps = np.empty(size)
+    bs_intercept_reps = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds,size=len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x,bs_y,1)
+
+    return bs_slope_reps, bs_intercept_reps
